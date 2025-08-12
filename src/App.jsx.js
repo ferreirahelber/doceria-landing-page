6b340e2c1db9eef496ab295ba50a6ad355d28e59
ferreirahelber <helberflopes@gmail.com>
2025-08-12 17:55:27 -0300

    Remoção de todo o background da página

--- src/App.jsx.js
+++ src/App.jsx.js
@@ -216,7 +216,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-pink-50 to-purple-50">
+    <div className="min-h-screen">
       
       {/* CABEÇALHO */}
       <header className="bg-header-background shadow-lg"> {/* A cor de fundo do cabeçalho é definida pela variável --header-background no App.css */}
